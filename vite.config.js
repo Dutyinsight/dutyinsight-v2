5ced415bbfc05aa45677fb77babbb40447c44016
Emrecan <emrecan.irmak@dutyinsight.com>
2026-05-16 12:46:33 +0300

    fix: completely removed old prerender plugin from vite config

--- vite.config.js
+++ vite.config.js
@@ -1,34 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import prerender from 'vite-plugin-prerender';
-import path from 'path';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    prerender({
-      staticDir: path.join(__dirname, 'dist'),
-      routes: [
-        '/',
-        '/tr', '/en', '/de', '/cs', '/pl',
-        '/tr/privacy', '/en/privacy', '/de/privacy', '/cs/privacy', '/pl/privacy',
-        '/tr/terms', '/en/terms', '/de/terms', '/cs/terms', '/pl/terms',
-        '/tr/cookies', '/en/cookies', '/de/cookies', '/cs/cookies', '/pl/cookies',
-      ],
-      postProcess(renderedRoute) {
-        renderedRoute.html = renderedRoute.html
-          .replace(/http:/g, 'https:')
-          .replace('id="root"', 'id="root" data-server-rendered="true"');
-        return renderedRoute;
-      },
-      renderer: '@prerenderer/renderer-puppeteer',
-      rendererOptions: {
-        renderAfterDocumentEvent: 'render-event',
-        maxConcurrentRoutes: 4,
-        headless: 'new',
-      },
-    }),
-  ],
+  plugins: [react()],
   build: {
     outDir: 'dist',
     sourcemap: false,
